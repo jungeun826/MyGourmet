491b75b058d529c73538de3d0ba21ab0463786dd
jungeun826 <jungeun826@nate.com>
2014-01-22 10:26:53 +0900

    IB를 이용한 UI를 구성하는 중이다.

---
+++ MyGourmet/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  MyGourmet
+//
+//  Created by SDT-1 on 2014. 1. 22..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ MyGourmet/TMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  TMapViewController.m
+//  MyGourmet
+//
+//  Created by SDT-1 on 2014. 1. 22..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import "TMapViewController.h"
+
+@interface TMapViewController ()
+
+@end
+
+@implementation TMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
