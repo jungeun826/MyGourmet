b0ca7ab41f21a5462b76a170161e40e00f8eea74
jungeun826 <jungeun826@nate.com>
2014-01-22 10:09:36 +0900

    Initial Commit

---
+++ MyGourmetTests/MyGourmetTests.m
@@ -0,0 +1,34 @@
+//
+//  MyGourmetTests.m
+//  MyGourmetTests
+//
+//  Created by SDT-1 on 2014. 1. 22..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyGourmetTests : XCTestCase
+
+@end
+
+@implementation MyGourmetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
